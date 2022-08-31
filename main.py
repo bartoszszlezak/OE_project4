60c08fcdbff47bfa527b030755bfa2f8a7043cdf
bartoszszlezak <64550654+bartoszszlezak@users.noreply.github.com>
2022-08-31 12:07:13 +0200

    Update procedure

--- main.py
+++ main.py
@@ -1,121 +1,179 @@
-import random
-from random import randint
+
 from deap import base
 from deap import creator
 from deap import tools
+import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn import model_selection
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
 
-from svc import SVCParameters, SVCParametersFitness
+from ada_boost import ABParametersFitness, ABParametersFeatures, mutationAB
+from algorithm import algorithm
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from decision_tree import DTParametersFeatures, DTParametersFitness, mutationDT
+from gaussian import GAUSSParametersFeatures, mutationGAUSS, GAUSSParametersFitness
+from mlp import MLPParametersFeatures, mutationMLP, MLPParametersFitness
+from random_forest import RFParametersFeatures, mutationRF, RFParametersFitness
+from svc import SVCParametersFitness, mutationSVC, SVCParametersFeatures
 
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
-def individual(icls):
-    genome = list()
-    for x in range(0, 40):
-        genome.append(randint(0, 1))
-    return icls(genome)
+# Konfiguracja
+# classifier = "SVC"
+classifier = "DT"
+# classifier = "GAUSS"
+# classifier = "RF"
+# classifier = "AB"
+# classifier = "MLP"
 
+# is_selection = True
+is_selection = False
 
-def fitnessFunction(individual):
-    # tutaj rozkoduj binarnego osobnika! Napisz funkcje decodeInd
-    ind = decodeInd(individual)
-    result = (ind[0] + 2 * ind[1] - 7) ** 2 + (2 * ind[0] + ind[1] - 5) ** 2
-    return result
+# file = "heart.csv"
+file = "data.csv"
 
+realRepresentation = True
+minimum = False
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
-
-toolbox = base.Toolbox()
-toolbox.register('individual', individual, creator.Individual)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-toolbox.register("evaluate", fitnessFunction)
-toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("mate", tools.cxOnePoint)
-
-# pd.set_option('display.max_columns', None)
-# df = pd.read_csv("data.csv", sep='\t')
-# y = df['Status']
-# df.drop('Status', axis=1, inplace=True)
-# df.drop('ID', axis=1, inplace=True)
-# df.drop('Recording', axis=1, inplace=True)
-# numberOfAtributtes = len(df.columns)
-# print(numberOfAtributtes)
-#
-# mms = MinMaxScaler()
-# df_norm = mms.fit_transform(df)
-# clf = SVC()
-# scores = model_selection.cross_val_score(clf, df_norm, y,
-#                                          cv=5, scoring='accuracy', n_jobs=-1)
-# print(scores.mean())
-#
-# toolbox.register('individual', SVCParameters, numberOfAtributtes, creator.Individual)
-# toolbox.register("evaluate", SVCParametersFitness, y, df, numberOfAtributtes)
-#
-# ind = ['poly', 0.3690320297276768, 1.9084110197644817, 0.1053757953826651,
-#        8.515094980694283]
-# print(SVCParametersFitness(y, df, numberOfAtributtes, ind))
-
-sizePopulation = 100
+sizePopulation = 10
 probabilityMutation = 0.2
 probabilityCrossover = 0.8
 numberIteration = 100
 
-pop = toolbox.population(n=sizePopulation)
-fitnesses = list(map(toolbox.evaluate, pop))
-for ind, fit in zip(pop, fitnesses):
-    ind.fitness.values = fit
+def plot(generations, val_array, title):
+    plt.title(title)
+    plt.plot([i for i in range(generations)], val_array, "o")
+    plt.show()
 
-while g < numberIteration:
-    g = g + 1
-    print("-- Generation %i --" % g)
-    # Select the next generation individuals
-    offspring = toolbox.select(pop, len(pop))
-    # Clone the selected individuals
-    offspring = list(map(toolbox.clone, offspring))
-    # Apply crossover and mutation on the offspring
-    for child1, child2 in zip(offspring[::2], offspring[1::2]):
-    # cross two individuals with probability CXPB
-    if random.random() < probabilityCrossover:
-        toolbox.mate(child1, child2)
-    # fitness values of the children
-    # must be recalculated later
-    del child1.fitness.values
-    del child2.fitness.values
-    for mutant in offspring:
-    # mutate an individual with probability MUTPB
-    if random.random() < probabilityMutation:
-        toolbox.mutate(mutant)
-    del mutant.fitness.values
-    # Evaluate the individuals with an invalid fitness
-    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-    fitnesses = map(toolbox.evaluate, invalid_ind)
-    for ind, fit in zip(invalid_ind, fitnesses):
+
+if __name__ == '__main__':
+
+    pd.set_option('display.max_columns', None)
+    df = pd.read_csv(file, sep=',')
+    if file == 'heart.csv':
+        y = df['target']
+        df.drop('target', axis=1, inplace=True)
+    else:
+        y = df['Status']
+        df.drop('Status', axis=1, inplace=True)
+        df.drop('ID', axis=1, inplace=True)
+        df.drop('Recording', axis=1, inplace=True)
+
+    mms = MinMaxScaler()
+    df_norm = mms.fit_transform(df)
+
+    if classifier == 'SVC':
+        clf = SVC()
+    elif classifier == 'DT':
+        clf = DecisionTreeClassifier()
+    elif classifier == 'GAUSS':
+        clf = GaussianProcessClassifier()
+    elif classifier == 'GAUSS':
+        clf = GaussianProcessClassifier()
+    elif classifier == 'RF':
+        clf = RandomForestClassifier()
+    elif classifier == 'AB':
+        clf = AdaBoostClassifier()
+    elif classifier == 'MLP':
+        clf = MLPClassifier()
+    else:
+        clf = SVC()
+
+    scores = model_selection.cross_val_score(clf, df_norm, y, cv=5, scoring='accuracy', n_jobs=-1)
+    print(f"Accuracy for default configuration: {scores.mean() * 100}%")
+
+    if is_selection:
+        if file == 'heart.csv':
+            df.drop('sex', axis=1, inplace=True)
+            df.drop('chol', axis=1, inplace=True)
+            df.drop('cp', axis=1, inplace=True)
+        else:
+            df.drop('MFCC1', axis=1, inplace=True)
+            df.drop('Jitter_rel', axis=1, inplace=True)
+            df.drop('Delta12', axis=1, inplace=True)
+
+    numberOfAtributtes = len(df.columns)
+    print(numberOfAtributtes)
+
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+
+    # Wybrać odpowiednie
+    if minimum:
+        creator.create("Individual", list, fitness=creator.FitnessMin)
+    else:
+        creator.create("Individual", list, fitness=creator.FitnessMax)
+
+    toolbox = base.Toolbox()
+
+    if classifier == 'SVC':
+        toolbox.register('individual', SVCParametersFeatures, numberOfAtributtes, creator.Individual)
+        toolbox.register("evaluate", SVCParametersFitness, y, df, numberOfAtributtes)
+        toolbox.register("mutate", mutationSVC)
+    elif classifier == 'DT':
+        toolbox.register('individual', DTParametersFeatures, numberOfAtributtes, creator.Individual)
+        toolbox.register("evaluate", DTParametersFitness, y, df, numberOfAtributtes)
+        toolbox.register("mutate", mutationDT)
+    elif classifier == 'GAUSS':
+        toolbox.register('individual', GAUSSParametersFeatures, numberOfAtributtes, creator.Individual)
+        toolbox.register("evaluate", GAUSSParametersFitness, y, df, numberOfAtributtes)
+        toolbox.register("mutate", mutationGAUSS)
+    elif classifier == 'RF':
+        toolbox.register('individual', RFParametersFeatures, numberOfAtributtes, creator.Individual)
+        toolbox.register("evaluate", RFParametersFitness, y, df, numberOfAtributtes)
+        toolbox.register("mutate", mutationRF)
+    elif classifier == 'AB':
+        toolbox.register('individual', ABParametersFeatures, numberOfAtributtes, creator.Individual)
+        toolbox.register("evaluate", ABParametersFitness, y, df, numberOfAtributtes)
+        toolbox.register("mutate", mutationAB)
+    elif classifier == 'MLP':
+        toolbox.register('individual', MLPParametersFeatures, numberOfAtributtes, creator.Individual)
+        toolbox.register("evaluate", MLPParametersFitness, y, df, numberOfAtributtes)
+        toolbox.register("mutate", mutationMLP)
+
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+    toolbox.register("mate", tools.cxTwoPoint)
+
+    pop = toolbox.population(n=sizePopulation)
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
-    print(" Evaluated %i individuals" % len(invalid_ind))
-    pop[:] = offspring
-    # Gather all the fitnesses in one list and print the stats
-    fits = [ind.fitness.values[0] for ind in pop]
-    length = len(pop)
-    mean = sum(fits) / length
-    sum2 = sum(x * x for x in fits)
-    std = abs(sum2 / length - mean ** 2) ** 0.5
-    print(" Min %s" % min(fits))
-    print(" Max %s" % max(fits))
-    print(" Avg %s" % mean)
-    print(" Std %s" % std)
-    best_ind = tools.selBest(pop, 1)[0]
-    print("Best individual is %s, %s" % (best_ind,
-                                         best_ind.fitness.values))
-#
-print("-- End of (successful) evolution --")
 
+    std_array, avg_array, fit_array, gen_array, best_ind = algorithm(numberIteration, probabilityMutation,
+                                                                     probabilityCrossover, toolbox, pop)
 
-def individual(icls):
-    genome = list()
-    genome.append(random.uniform(-10, 10))
-    genome.append(random.uniform(-10, 10))
-    return icls(genome)
+    mms = MinMaxScaler()
+    df_norm = mms.fit_transform(df)
+
+    if classifier == 'SVC':
+        clf = SVC(kernel=best_ind[0], C=best_ind[1], degree=best_ind[2], gamma=best_ind[3],
+                  coef0=best_ind[4], random_state=101)
+    elif classifier == 'DT':
+        clf = DecisionTreeClassifier(criterion=best_ind[0], splitter=best_ind[1], max_depth=best_ind[2],
+                                     min_samples_split=best_ind[3], max_features=best_ind[4], random_state=101)
+    elif classifier == 'GAUSS':
+        clf = GaussianProcessClassifier(kernel=best_ind[0], n_restarts_optimizer=best_ind[1],
+                                        max_iter_predict=best_ind[2], random_state=101)
+    elif classifier == 'RF':
+        clf = RandomForestClassifier(n_estimators=best_ind[0], criterion=best_ind[1], max_depth=best_ind[2],
+                                     max_features=best_ind[3], random_state=101)
+    elif classifier == 'AB':
+        clf = AdaBoostClassifier(n_estimators=best_ind[0], learning_rate=best_ind[1], algorithm=best_ind[2],
+                                 random_state=101)
+    elif classifier == 'MLP':
+        clf = MLPClassifier(activation=best_ind[0], solver=best_ind[1], alpha=best_ind[2],
+                            max_iter=best_ind[3], random_state=101)
+
+    scores = model_selection.cross_val_score(clf, df_norm, y, cv=5, scoring='accuracy', n_jobs=-1)
+
+    if is_selection:
+        print("Accuracy after optimalisation with selection ")
+    print(f"Accuracy after optimalisation: {scores.mean() * 100}%")
+
+    plot(numberIteration, std_array, "Odchylenie standardowe w kolejnej iteracji")
+    plot(numberIteration, avg_array, "Średnia w kolejnej iteracji")
+    plot(numberIteration, fit_array, "Funkcja celu w kolejnej iteracji")
--- svc.py
+++ svc.py
@@ -1,32 +1,9 @@
-import pandas as pd
-
-# pd.set_option('display.max_columns', None)
-# df = pd.read_csv("data.csv", sep='\t')
-# y = df['Status']
-# df.drop('Status', axis=1, inplace=True)
-# df.drop('ID', axis=1, inplace=True)
-# df.drop('Recording', axis=1, inplace=True)
-# numberOfAtributtes = len(df.columns)
-# print(numberOfAtributtes)
-
-from sklearn import model_selection
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.model_selection import StratifiedKFold
-from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
-
-# mms = MinMaxScaler()
-# df_norm = mms.fit_transform(df)
-# clf = SVC()
-# scores = model_selection.cross_val_score(clf, df_norm, y,
-#                                          cv=5, scoring='accuracy', n_jobs=-1)
-# print(scores.mean())
+from sklearn import metrics
 
 import random
-
-from sklearn.tree import DecisionTreeClassifier
 
 
 def SVCParameters(numberFeatures, icls):
@@ -48,10 +25,6 @@ def SVCParameters(numberFeatures, icls):
     return icls(genome)
 
 
-import math
-from sklearn import metrics
-
-
 def SVCParametersFitness(y, df, numberOfAtributtes, individual):
     split = 5
     cv = StratifiedKFold(n_splits=split)
@@ -66,41 +39,9 @@ def SVCParametersFitness(y, df, numberOfAtributtes, individual):
     expected = y[test]
     tn, fp, fn, tp = metrics.confusion_matrix(expected, predicted).ravel()
     result = (tp + tn) / (
-                tp + fp + tn + fn)  # w oparciu o macierze pomyłekhttps: // www.dataschool.io / simple - guide - to - confusion - matrixterminology /
+            tp + fp + tn + fn)  # w oparciu o macierze pomyłekhttps: // www.dataschool.io / simple - guide - to - confusion - matrixterminology /
     resultSum = resultSum + result  # zbieramy wyniki z poszczególnych etapów walidacji krzyżowej
     return resultSum / split,
-
-
-# def mutationSVC(individual):
-#     numberParamer = random.randint(0, len(individual) - 1)
-#     if numberParamer == 0:
-#         # kernel
-#         listKernel = ["linear", "rbf", "poly", "sigmoid"]
-#         individual[0] = listKernel[random.randint(0, 3)]
-#     elif numberParamer == 1:
-#         # C
-#         k = random.uniform(0.1, 100)
-#         individual[1] = k
-#     elif numberParamer == 2:
-#         # degree
-#         individual[2] = random.uniform(0.1, 5)
-#     elif numberParamer == 3:
-#         # gamma
-#         gamma = random.uniform(0.01, 5)
-#         individual[3] = gamma
-#     elif numberParamer == 4:
-#         # coeff
-#         coeff = random.uniform(0.1, 20)
-#         individual[2] = coeff
-
-
-# # toolbox.register('individual', SVCParameters, numberOfAtributtes, creator.Individual)
-# # toolbox.register("evaluate", SVCParametersFitness, y, df, numberOfAtributtes)
-#
-#
-# ind = ['poly', 0.3690320297276768, 1.9084110197644817, 0.1053757953826651,
-#        8.515094980694283]
-# print(SVCParametersFitness(y, df, numberOfAtributtes, ind))
 
 
 def SVCParametersFeatures(numberFeatures, icls):
@@ -140,17 +81,6 @@ def SVCParametersFeatureFitness(y, df, numberOfAtributtes, individual):
     estimator = SVC(kernel=individual[0], C=individual[1], degree=individual[2], gamma=individual[3],
                     coef0=individual[4], random_state=101)
 
-
-
-    # estimator = DecisionTreeClassifier()
-    # estimator = AdaBoostClassifier()
-    # estimator = GradientBoostingClassifier()
-    # estimator = GaussianNB()
-    # estimator = LinearDiscriminantAnalysis()
-
-
-
-
     resultSum = 0
     for train, test in cv.split(df_norm, y):
         estimator.fit(df_norm[train], y[train])
@@ -159,7 +89,7 @@ def SVCParametersFeatureFitness(y, df, numberOfAtributtes, individual):
     tn, fp, fn, tp = metrics.confusion_matrix(expected,
                                               predicted).ravel()
     result = (tp + tn) / (
-                tp + fp + tn + fn)  # w oparciu o macierze pomyłek https: // www.dataschool.io / simple - guide - to - confusion - matrixterminology /
+            tp + fp + tn + fn)  # w oparciu o macierze pomyłek https: // www.dataschool.io / simple - guide - to - confusion - matrixterminology /
     resultSum = resultSum + result  # zbieramy wyniki z poszczególnych etapów walidacji krzyżowej
     return resultSum / split,
 
